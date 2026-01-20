b48fde7ccdcf89d6ee1f726123e4e8a36d52999b
gujc@aarhus.dk <nruXH246>
2026-01-20 11:18:17 +0100

    ref

--- robot_framework/process.py
+++ robot_framework/process.py
@@ -429,6 +429,7 @@ def process(orchestrator_connection: OrchestratorConnection, queue_element: Queu
             ref_navn = str(row.get(col_ref_navn)).strip()
              #  CLEANUP STEP 
             ref_navn = re.sub(r'(?i)\b(att:|att\.|til:)\b', '', ref_navn).strip()
+            print(f"Cleaned refnavn er: {ref_navn}")
             
             faktura_nummer = row.get("Fakturabilag")
             AktueltBilagsDato = row.get(col_bilagsdato)
